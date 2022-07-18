570ac2f254df5083c90786722c0e187b133e9d34
Alan Andrade <alan@fasters.com.br>
2022-07-18 15:10:08 -0300

    aula02 stubs

---
+++ aula02-stubs/src/service.js
@@ -0,0 +1,29 @@
+const https = require('https');
+
+class Service {
+  async makeRequest(url) {
+    return new Promise((resolve, reject) => {
+      https.get(url, response => {
+        response.on('data', data => resolve(JSON.parse(data)))
+        response.on('error', reject)
+      });
+    })
+  };
+
+  async getPlanets(url) {
+    const result = await this.makeRequest(url);
+
+    return {
+      name: result.name,
+      surfaceWater: result.surface_water,
+      appearedIn: result.films.length
+    }
+  }
+}
+
+module.exports = Service;
+
+// (async () => {
+//   const response = await new Service().makeRequest('https://swapi.dev/api/planets/1/');
+//   console.log('response', response);
+// })();
